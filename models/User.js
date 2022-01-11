00c4fe3ff8672b3639edf18c448fffc8d496d4ac
NickNocc <haretakis4@gmail.com>
2022-01-11 15:48:50 -0800

    added password check

--- models/User.js
+++ models/User.js
@@ -3,7 +3,13 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+
+}
 
 // define table columns and configuration
 User.init(
@@ -39,8 +45,12 @@ User.init(
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
-        }
+        },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
       },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
