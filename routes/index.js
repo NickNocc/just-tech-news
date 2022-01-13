0f9204617d1b05681f031b667011125a96c88769
NickNocc <haretakis4@gmail.com>
2022-01-13 15:40:35 -0800

    as

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/comments', commentRoutes)
 
 router.use((req, res) => {
   res.status(404).end();
